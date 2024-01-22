e7597376f8c4eeb28372dc5b655d9b0d8fe74a03
Adam Gąsowski <gander@users.noreply.github.com>
2024-01-22 20:42:38 +0100

    remove lastUpdated

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,7 +2,6 @@ import {defineConfig} from 'vitepress';
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-    lastUpdated: true,
     title: 'Gander Site',
     description: 'Code solutions, tips and tricks.',
     srcDir: 'docs',
