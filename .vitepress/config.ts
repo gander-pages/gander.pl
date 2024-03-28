7fc8b542286d4c511bde0331c5f3abb1bfd40ebe
Adam Gąsowski <gander@users.noreply.github.com>
2024-03-28 13:06:33 +0100

    build: add Tianji

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -22,6 +22,18 @@ export default defineConfig({
             ]);
         }
 
+        if (process.env.VITE_TIANJI_ID && process.env.VITE_TIANJI_SRC) {
+            scripts.push([
+                'script',
+                {
+                    'async': 'true',
+                    'defer': 'true',
+                    'data-website-id': process.env.VITE_TIANJI_ID,
+                    'src': process.env.VITE_TIANJI_SRC,
+                }
+            ]);
+        }
+
         if (process.env.SENTRY_DSN) {
             scripts.push([
                 'script',
