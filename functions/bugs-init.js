838de4f5033f2925e6c44623b4d2a1fca64a130d
Adam Gąsowski <gander@users.noreply.github.com>
2024-03-28 14:25:51 +0100

    build!: change Sentry env name

--- functions/bugs-init.js
+++ functions/bugs-init.js
@@ -1,4 +1,4 @@
 export async function onRequestGet({env}) {
-    return new Response(`Sentry.init({dsn: '${env.SENTRY_DSN}',tunnel: '/bugs-tunnel'});`, {headers: [['content-type', 'text/javascript']]});
+    return new Response(`Sentry.init({dsn: '${env.VITE_SENTRY_DSN}',tunnel: '/bugs-tunnel'});`, {headers: [['content-type', 'text/javascript']]});
 }
 
